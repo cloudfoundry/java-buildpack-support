fe3cf21c65287129c6f6eb364dbdfb79d72f4a0c
Ben Hale <bhale@pivotal.io>
2017-07-17 11:53:42 -0700

    Support Tomcat 8+
    This change adds additional methods to the Access Logging Valve implementation
    to make it compatible with Tomcat 8.
    
    [#463]

--- tomcat-access-logging-support/src/main/java/com/gopivotal/cloudfoundry/tomcat/logging/access/CloudFoundryAccessLoggingValve.java
+++ tomcat-access-logging-support/src/main/java/com/gopivotal/cloudfoundry/tomcat/logging/access/CloudFoundryAccessLoggingValve.java
@@ -18,6 +18,8 @@ package com.gopivotal.cloudfoundry.tomcat.logging.access;
 
 import org.apache.catalina.valves.AccessLogValve;
 
+import java.io.CharArrayWriter;
+
 /**
  * An extension of {@link org.apache.catalina.valves.AccessLogValve} that logs to {@link System#out}.
  */
@@ -28,9 +30,11 @@ public final class CloudFoundryAccessLoggingValve extends AccessLogValve {
      *
      * @param message the message to log
      */
-    @SuppressWarnings("PMD.SystemPrintln")
-    @Override
     public void log(String message) {
+        System.out.println(message);
+    }
+
+    public void log(CharArrayWriter message) {
         System.out.println(message);
     }
 
